6d2577a9f6a10002b24c8660f69dd8df1d342cd4
Max <maxim.bigin@yandex.ru>
2021-05-08 21:12:59 +0300

    edits in settings
    add STATIC_ROOT and folders templates

--- flagraph/settings.py
+++ flagraph/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'flagraph.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'core', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
