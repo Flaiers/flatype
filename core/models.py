2c00b9e6dd2c5a677c761dd3c72e84ac1acd3614
Max <maxim.bigin@yandex.ru>
2021-05-09 21:32:44 +0300

    model article ans views in admin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        #return slugify(self.title)
+        return self.title
