f715c7e69dc6bae7b7544f4e25696f2330051824
Max <maxim.bigin@yandex.ru>
2021-05-10 01:04:22 +0300

    dict for slugify and views for create

--- core/views.py
+++ core/views.py
@@ -1,5 +1,25 @@
+from .env import alphabet
+from .forms import ArticleForm
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return render(request, 'create.html')
+    return HttpResponse('Hello, World!')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.slug = slugify(''.join(alphabet.get(w, w) for w in article.title.lower()))
+            article.author = request.user
+            article.save()
+            return HttpResponse('Hello, World!')
+    else:
+        form = ArticleForm()
+
+    return render(request, 'create.html', {'form': form})
