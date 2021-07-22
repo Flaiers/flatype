ea75ffe6f802b2be17abf155ff1987c598fdf3b4
Max <BiginMS@stud.kai.ru>
2021-07-22 20:49:29 +0300

    slug save in model

--- core/views.py
+++ core/views.py
@@ -1,5 +1,4 @@
 import environ
-from datetime import date
 from .models import Article
 from .forms import ArticleForm
 from django.shortcuts import render
@@ -21,8 +20,6 @@ def create_new(request):
 
         if form.is_valid():
             article = form.save(commit=False)
-            date = article.date
-            article.slug = slugify(''.join(eval(env('ALPHABET')).get(w, w) for w in article.title.lower())) + date.strftime('-%m-%d')
             article.author = request.user
             article.save()
             return HttpResponse('Hello, World!')
