7a080dadf4d9b77c75792a7b12e80d521c092301
Max <bigin.maks@gmail.com>
2021-08-05 17:59:08 +0300

    add nginx

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.9
 ENV PYTHONUNBUFFERED=1
 
-WORKDIR /flatype
-COPY . /flatype/
+WORKDIR /usr/projects/flatype
+COPY . /usr/projects/flatype/
 RUN pip install -U pip && pip install --no-cache-dir -r requirements.txt
--- flatype/settings.py
+++ flatype/settings.py
@@ -130,9 +130,6 @@ USE_TZ = env('USE_TZ')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
