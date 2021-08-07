53b5e7636c8c88b8da6b6d8bff59749fc03b7af3
Max <bigin.maks@gmail.com>
2021-08-07 17:31:48 +0300

    view session in admin

--- ext_auth/admin.py
+++ ext_auth/admin.py
@@ -1,10 +1,18 @@
-from django.contrib import admin
-
 from .models import ExternalHashId
+
+from django.contrib import admin
+from django.contrib.sessions.models import Session
 from django.contrib.auth import get_user_model, admin as auth_admin
 
 
 actual_user_model = get_user_model()
+
+
+class SessionAdmin(admin.ModelAdmin):
+    get_session_data = lambda self, obj: obj.get_decoded()
+    get_session_data.short_description = 'session data'
+
+    list_display = ['session_key', 'get_session_data', 'expire_date']
 
 
 class ExternalHashIdInline(admin.TabularInline):
@@ -21,5 +29,6 @@ class UserAdmin(auth_admin.UserAdmin):
 
 admin.site.unregister(actual_user_model)
 admin.site.register(actual_user_model, UserAdmin)
+admin.site.register(Session, SessionAdmin)
 
 admin.register(ExternalHashId)
