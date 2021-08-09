9b9901fd16fe93e91e88a3f8c166b23dacb5b4fd
Max <bigin.maks@gmail.com>
2021-08-10 00:25:44 +0300

    fix up project

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 python_interpreter=""
 
-read -p "Python interpreter: " python_interpreter
-`$python_interpreter -m venv env`
-source env/bin/activate
+read -p "----------------------
+| Python interpreter |
+----------------------
+Default: /usr/bin/python3
+If you wont to change, write: " python_interpreter
 
-pip install -U pip
-pip install -r requirements.txt
+
+if [ -z "$python_interpreter" ]; then
+    /usr/bin/python3 -m venv env
+else
+    `$python_interpreter -m venv env`
+fi
+
+source env/bin/activate
+pip install -U pip && pip install --no-cache-dir -r requirements.txt
 
 cd src/
 
