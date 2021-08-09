f7bc44319980d9884b6b0cd296de8c8961475a41
Max <bigin.maks@gmail.com>
2021-08-09 18:47:46 +0300

    insecure run

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ source env/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 
-cd src
+cd src/
 
 python manage.py collectstatic
 python manage.py migrate
-python manage.py runserver
+python manage.py runserver --insecure
