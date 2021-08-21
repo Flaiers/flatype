04fd36da8d6d4feb55e92e042423baa222a240f9
Max <bigin.maks@gmail.com>
2021-08-22 01:56:46 +0300

    view page clear

--- src/core/views.py
+++ src/core/views.py
@@ -22,39 +22,10 @@ class View(TemplateView):
         except Exception as e:
             return page_not_found(request, e)
 
-        owner_hash = request.session.get('externalid')
-        if request.GET.get('edit', False) and \
-            (request.user == article.owner or owner_hash == article.owner_hash):
-
-            form = ArticleForm(instance=article)
-
-            return render(request, 'write.html',
-                {
-                    'form': form
-                }
-            )
-
         return render(request, 'view.html',
             {
                 'article': article,
-                'owner_hash': owner_hash,
                 'content': format_html(article.text),
                 'date': article.date.strftime('%B %d, %Y'),
             }
         )
-
-    def post(self, request, slug: str, *args, **kwargs):
-        try:
-            article = Article.objects.get(slug=slug)
-        except Exception as e:
-            return page_not_found(request, e)
-
-        owner_hash = request.session.get('externalid')
-        if request.GET.get('edit', False) and \
-            (request.user == article.owner or owner_hash == article.owner_hash):
-
-            form = ArticleForm(request.POST)
-            if form.is_valid():
-                article = try_edit(request, article)
-
-                return redirect('view', slug=article.slug)
