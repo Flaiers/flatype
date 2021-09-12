024872070cb4629e45a88aee669ffa37b52c3434
Max <bigin.maks@gmail.com>
2021-09-12 16:27:57 +0300

    fix syntax and url

--- src/ext_auth/urls.py
+++ src/ext_auth/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('register', views.try_register, name='register'),
     path('logout', views.try_logout, name='logout'),
     path('login', views.try_login, name='login'),
     path('check', views.try_check, name='check'),
