a5750e99dfe4d60810d8300730f2bfbc5a7da6ce
Max <bigin.maks@gmail.com>
2021-09-14 17:18:30 +0300

    model User fix fields

--- src/ext_auth/models.py
+++ src/ext_auth/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    link = models.URLField('User link', max_length=200, blank=True, null=True,
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField('email address', blank=True, null=True)
+    link = models.URLField('user link', max_length=200, blank=True, null=True,
         help_text='150 characters or fewer. Link to account on the site.'
     )
 
