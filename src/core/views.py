1aa67bfe9791491ca918b88610077d4ce4f960e6
Max <bigin.maks@gmail.com>
2021-09-15 17:13:11 +0300

    render with safe, and use standard context data

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,5 @@
 from .models import Article
 
-from django.utils.html import format_html
 from django.views.generic import TemplateView, DetailView
 
 
@@ -14,13 +13,3 @@ class View(DetailView):
     model = Article
     queryset = Article.objects.all()
     template_name = 'view.html'
-
-    def get_context_data(self, *args, **kwargs):
-        article = super().get_context_data(*args, **kwargs)['article']
-        context = {
-            'title': article.title,
-            'author': article.author if article.author is not None else '',
-            'date': article.date,
-            'content': format_html(article.content)
-        }
-        return context
