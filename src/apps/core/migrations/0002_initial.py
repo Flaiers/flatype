c7fd235b415ec82110eb4c8303dbd1b0f7dee635
Max <bigin.maks@gmail.com>
2021-11-05 22:05:59 +0300

    fix exception

--- src/apps/core/migrations/0002_initial.py
+++ src/apps/core/migrations/0002_initial.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('sessions', '0002_alter_db_table'),
+        ('sessions', '0001_initial'),
         ('core', '0001_initial'),
     ]
 
--- src/apps/ext_auth/migrations/0001_initial.py
+++ src/apps/ext_auth/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0013_alter_db_table'),
+        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
