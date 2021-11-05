c11e67eb6e8f073b476d13f42d87c1c19d76e6ab
Max <bigin.maks@gmail.com>
2021-11-05 21:49:38 +0300

    fix migrations

--- src/apps/core/migrations/0001_initial.py
+++ src/apps/core/migrations/0001_initial.py
@@ -1,7 +1,5 @@
 import datetime
-from django.conf import settings
 from django.db import migrations, models
-import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -9,11 +7,25 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('sessions', '0002_alter_db_table'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=150)),
+                ('slug', models.SlugField(blank=True, unique=True)),
+                ('author', models.CharField(blank=True, max_length=64, null=True)),
+                ('content', models.TextField()),
+                ('date', models.DateField(default=datetime.date.today)),
+            ],
+            options={
+                'verbose_name': 'Article',
+                'verbose_name_plural': 'Articles',
+                'db_table': 'articles',
+            },
+        ),
         migrations.CreateModel(
             name='Storage',
             fields=[
@@ -27,24 +39,6 @@ class Migration(migrations.Migration):
                 'verbose_name': 'Storage object',
                 'verbose_name_plural': 'Storage',
                 'db_table': 'storage',
-            },
-        ),
-        migrations.CreateModel(
-            name='Article',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=150)),
-                ('slug', models.SlugField(blank=True, unique=True)),
-                ('author', models.CharField(blank=True, max_length=64, null=True)),
-                ('content', models.TextField()),
-                ('date', models.DateField(default=datetime.date.today)),
-                ('owner', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-                ('owner_sessions', models.ManyToManyField(blank=True, db_table='article_owners', to='sessions.Session')),
-            ],
-            options={
-                'verbose_name': 'Article',
-                'verbose_name_plural': 'Articles',
-                'db_table': 'articles',
             },
         ),
     ]
---
+++ src/apps/core/migrations/0002_initial.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('sessions', '0002_alter_db_table'),
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='article',
+            name='owner',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='owner_sessions',
+            field=models.ManyToManyField(blank=True, db_table='article_owners', to='sessions.Session'),
+        ),
+    ]
