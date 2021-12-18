f8b2f7b05de9f1724a91790c11f22bfa225d8b60
Max <bigin.maks@gmail.com>
2021-12-18 14:03:02 +0300

    core admin

---
+++ src/apps/core/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+from .models import Article, Storage
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('title', 'slug', 'author', 'content', 'date')}),
+        ('Permissions', {'fields': ('owner', 'owner_sessions')}),
+    )
+    list_display = ('title', 'slug', 'author', 'date',)
+    filter_horizontal = ('owner_sessions',)
+    search_fields = ('title', 'content',)
+    date_hierarchy = 'date'
+    ordering = ('-date',)
+
+    def get_readonly_fields(self, request, obj=None) -> tuple:
+        if obj:
+            return self.readonly_fields + ('slug', 'owner', 'date',)
+        return self.readonly_fields
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+
+    fields = ('hash', 'file', 'date', 'use_hash',)
+    search_fields = ('file', 'hash', 'date',)
+    list_display = ('file', 'hash', 'date',)
+    list_display_links = ('date',)
+    date_hierarchy = 'date'
+    ordering = ('-date',)
+
+    def get_readonly_fields(self, request, obj=None) -> tuple:
+        if obj:
+            return self.readonly_fields + ('hash', 'file', 'date', 'use_hash',)
+        return self.readonly_fields
